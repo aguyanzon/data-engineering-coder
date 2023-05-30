9dc918236a206747e6717990836b88b73611193d
Agustin Yanzon <aguyanzon@hotmail.com>
2023-05-29 22:08:40 -0300

    upload files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# data-engineering-coder
-Curso de Ingeniería de Datos - CoderHouse
+# Entregable 1
+## Consigna
+Desarrollar un script que extraiga datos de una API pública. A su vez, se debe crear una tabla en Redshift y cargar los datos extraídos.
+
+## Desarrollo
+Para el siguiente trabajo se extrajeron los datos de
+[Alpha Vantage](https://www.alphavantage.co/) que proporciona datos del mercado financiero de nivel empresarial.
+
+La extracción se realizó vía API y se tomaron los valores mensuales (último día de negociación de cada mes, apertura mensual, máximo mensual, mínimo mensual, cierre mensual, volumen mensual) de las acciones de IBM (IBM), APPLE (AAPL) y TESLA (TSLA), que cubre más de 20 años de datos históricos.
+
+Luego se realizó la transformación de los datos y en última instancia se creó una tabla en Redshift y la posterior carga de los datos a la misma.
+
+En la siguiente imagen se puede observar la salida de la tabla consultada desde Dbeaver.
+
+<img src="images/output_query.png">
---
+++ finance.py
@@ -0,0 +1,106 @@
+import os
+
+import pandas as pd
+import redshift_connector
+import requests
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+def extract_data(symbol):
+    try:
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey={os.getenv("API_KEY")}'
+        response = requests.get(url)
+        json_data = response.json()
+        data = pd.DataFrame(json_data['Monthly Time Series'])
+        data = data.T
+        data['symbol'] = symbol
+        
+        return data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error de solicitud: {e}")
+        return None
+
+def transform_data(data):
+    data.rename(columns = {
+        "1. open" : "open", 
+        "2. high" : "high", 
+        "3. low" : "low", 
+        "4. close" : "close", 
+        "5. volume" : "volume"
+        }, inplace=True)
+    for column in data[["open", "high", "low", "close", "volume"]]:
+        data[column] = data[column].astype(float)
+
+    data.reset_index(inplace=True)
+    data.rename(columns={"index":"date"}, inplace=True)
+    data.date = pd.to_datetime(data.date)
+
+    return data
+
+def load_data(data):
+    # Nombre de la tabla y esquema
+    tabla = "finance_ibm"
+    esquema = "aguyanzon_coderhouse"
+
+    # Crea la conexión a Redshif
+    print("Conectando a Redshift...")
+    conn = redshift_connector.connect(
+        host=os.getenv("REDSHIFT_HOST"),
+        port=int(os.getenv("REDSHIFT_PORT")),
+        database=os.getenv("REDSHIFT_DATABASE"),
+        user=os.getenv("REDSHIFT_USER"),
+        password=os.getenv("REDSHIFT_PASSWORD")
+    )
+    print('Conexion a Redshift creada...')
+
+    # Crea la tabla en el esquema especificado
+    create_table_query = f'''
+        CREATE TABLE {esquema}.{tabla} (
+            "date" DATE,
+            "open" VARCHAR(255),
+            high VARCHAR(255),
+            low VARCHAR(255),
+            close VARCHAR(255), 
+            volume VARCHAR(255),
+            symbol VARCHAR(255)
+        )
+    '''
+    drop_table_query = f'DROP TABLE IF EXISTS {esquema}.{tabla}'
+
+    with conn.cursor() as cursor:
+        cursor.execute(drop_table_query)
+        print(f"Creando tabla {tabla}")
+        cursor.execute(create_table_query)
+        for index, row in data.iterrows():
+            print(f"Insertando fila {index + 1} de {len(data)}")
+            cursor.execute(
+                f'''INSERT INTO {esquema}.{tabla} ("date", "open", high, low, close, volume, symbol) VALUES (%s, %s, %s, %s, %s, %s, %s)''',
+                (row['date'], row['open'], row['high'], row['low'], row['close'], row['volume'], row['symbol'])
+            )
+    conn.commit()
+
+    # Cierra la conexión a Redshift
+    conn.close()
+
+if __name__ == "__main__":
+
+    print("Extrayendo datos...")
+    data_ibm = extract_data('IBM')
+    data_aapl = extract_data('AAPL')
+    data_tsla = extract_data('TSLA')
+    data = pd.concat([data_ibm, data_aapl, data_tsla], axis=0)
+
+    if data is not None:
+        print("Datos extraídos exitosamente.")
+
+        print("Transformando datos...")
+        transformed_data = transform_data(data)
+        print("Datos transformados exitosamente.")
+
+        print("Cargando datos en Redshift...")
+        load_data(transformed_data)
+        print("Datos cargados exitosamente.")
+
+
